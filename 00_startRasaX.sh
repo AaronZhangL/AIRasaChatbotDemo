58efd55f90873f0ff5a508d23e916c80ebf76f12
l.zhang <l.zhang@linecorp.com>
2021-09-18 03:40:56 +0900

    Update

--- 00_startRasaX.sh
+++ 00_startRasaX.sh
@@ -1,3 +1,4 @@
 # login url: http://localhost:5002/login\?username\=me\&password\=xxxxx
+echo "Use command : sh ./00_startRasaX.sh {your-password}"
 RASA_X_PASSWORD=$1 rasa x
 
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # AIRasaChatbotDemo
 A Chatbot demo base on RASA frame  
-(Base on https://github.com/BSlience/rasa-conversational-ai)
 
 ## Init Rasa
 
